8f673b23aca77504bdfb936aa18aa0c7ade52497
Tom <thomasaeh1@gmail.com>
2020-09-27 23:37:14 +0100

    added randomforest crate and added todos

--- src/ml_coefficients_estimator/lift_coeff_estimator/mod.rs
+++ src/ml_coefficients_estimator/lift_coeff_estimator/mod.rs
@@ -1,5 +1,67 @@
 // TODO: make a machine learning module here that predicts the lift coefficient from a 3D model given by the user (STL (with a .STL extension), COLLADA (with a .DAE extension), etc)
 
-pub fn predict_lift_coefficient(data: & <f64>) -> f64 {
+use std::fs::File;
 
+use randomforest::{RandomForestRegressor, RandomForestRegressorOptions};
+use randomforest::criterion::Mse;
+use randomforest::table::{Table, TableBuilder};
+
+const MODEL_FILE_PATH: &str = "data/lift_coefficient_model";
+
+const TRAINING_INPUTS: [&[f64]; 12] = [
+    &[0.0, 2.0, 1.0, 0.0][..],
+    &[0.0, 2.0, 1.0, 1.0][..],
+    &[1.0, 2.0, 1.0, 0.0][..],
+    &[2.0, 1.0, 1.0, 0.0][..],
+    &[2.0, 0.0, 0.0, 0.0][..],
+    &[2.0, 0.0, 0.0, 1.0][..],
+    &[1.0, 0.0, 0.0, 1.0][..],
+    &[0.0, 1.0, 1.0, 0.0][..],
+    &[0.0, 0.0, 0.0, 0.0][..],
+    &[2.0, 1.0, 0.0, 0.0][..],
+    &[0.0, 1.0, 0.0, 1.0][..],
+    &[1.0, 1.0, 1.0, 1.0][..],
+];
+
+const TRAINING_OUTPUTS: [f64; 12] = [
+    25.0, 30.0, 46.0, 45.0, 52.0, 23.0, 43.0, 35.0, 38.0, 46.0, 48.0, 52.0
+];
+
+//TODO: get real training and testing data and measure performance
+
+fn get_random_forest_regressor() -> RandomForestRegressor {
+    match load_random_forest_regressor() {
+        Some(regressor) => regressor,
+        None => make_random_forest_regressor()
+    }
+}
+
+fn make_random_forest_regressor() -> RandomForestRegressor {
+    //TODO: do this properly
+    let mut table_builder: TableBuilder = TableBuilder::new();
+    for (xs, y) in TRAINING_INPUTS.iter().zip(TRAINING_OUTPUTS.iter()) {
+        table_builder.add_row(xs, *y)?;
+    }
+    let table: Table = table_builder.build()?;
+
+    let regressor: RandomForestRegressor = RandomForestRegressorOptions::new()
+        .seed(0)
+        .fit(Mse, table);
+
+    save_random_forest_regressor(&regressor);
+
+    regressor
+}
+
+fn load_random_forest_regressor() -> Option<RandomForestRegressor> {
+    RandomForest::deserialize(File::open(MODEL_FILE_PATH))
+}
+
+fn save_random_forest_regressor(regressor: &RandomForestRegressor) {
+    let writer: File = File::create(MODEL_FILE_PATH)?;
+    regressor.serialize(writer).unwrap();
+}
+
+pub fn predict_lift_coefficient(data: &[f64]) -> f64 {
+    get_random_forest_regressor().predict(data)
 }
--- src/ml_coefficients_estimator/mod.rs
+++ src/ml_coefficients_estimator/mod.rs
@@ -5,17 +5,18 @@ use stl_io::IndexedMesh;
 mod drag_coeff_estimator;
 mod lift_coeff_estimator;
 
-pub fn process_stl_file(stl_file_path: &str) -> {
+pub fn process_stl_file(stl_file_path: &str) -> &[f64] {
     let mut file = OpenOptions::new().read(true).open(stl_file_path).unwrap();
     let stl: IndexedMesh = stl_io::read_stl(&mut file).unwrap();
     let rows: usize = stl.faces.len(); // num of triangles
     let cols: usize = 9; // 3x vertices per triangle, 3x coords per vertex
+    //TODO: finish this
 }
 
-pub fn get_drag_coefficient(processed_stl_file: & <f64>) -> f64 {
-drag_coeff_estimator::predict_drag_coefficient(processed_stl_file)
+pub fn get_drag_coefficient(processed_stl_file: &[f64]) -> f64 {
+    drag_coeff_estimator::predict_drag_coefficient(processed_stl_file)
 }
 
-pub fn get_lift_coefficient(processed_stl_file: & < f64 >) -> f64 {
-lift_coeff_estimator::predict_lift_coefficient(processed_stl_file)
+pub fn get_lift_coefficient(processed_stl_file: &[f64]) -> f64 {
+    lift_coeff_estimator::predict_lift_coefficient(processed_stl_file)
 }
